c8f1e0ccb70f0bd2a3f8810e7d2063b8dd65e980
fcrozatier <frederic.crozatier@protonmail.com>
2025-07-25 10:49:29 +0200

    remove parser

--- client/html.ts
+++ client/html.ts
@@ -1,84 +1,10 @@
-import { isSignal } from "../client/signals.ts";
+import { effect, isSignal } from "../client/signals.ts";
 import type { TemplateTag } from "../definitions.d.ts";
+import { assertExists } from "./assert.ts";
+import { type Attachment, isAttachment } from "./attachement.ts";
 import { Boundary } from "./Boundary.ts";
 import { isComponent } from "./component.ts";
-
-// data
-const text = /[^<]+/y;
-const rawTextElements = /^(?:script|style|textarea|title)$/i;
-const rawText =
-  /^(?:(?!<\/(?:script|style|textarea|title)[\t\n\f\r\u0020>/]).|\n)*/i;
-const comment = /<!--((?!-->)\p{Any})*-->/vy;
-const cdata = /<!\[CDATA\[(?:(?!\]\]>)\p{Any})*\]\]>/vy;
-
-// element
-const openTag = /<(?<tagname>[^\s>]+)\s+/vy;
-const attribute = /[^\s"'>\/=]+\s*=\s*/y;
-const booleanAttribute = /[^\s"'>\/=]+\s*/y;
-const attributeValueQuoted = /(['"])(.*?)\1\s*/y;
-const attributeValueUnquoted = /[^\s='"<>`]+\s*/y;
-const closeOpenTag = /\/?>/y;
-const endTag = /<\/(?<tagname>[^\s]+)>/y;
-
-let rawTextMode = false;
-let state: RegExp | null = null;
-let match: RegExpExecArray | null = null;
-
-const incrementalParse = (input: string) => {
-  let pos = 0;
-  let admissibleNextStates: RegExp[] = [];
-
-  const nextState = () => {
-    for (const regex of admissibleNextStates) {
-      regex.lastIndex = pos;
-      match = regex.exec(input);
-
-      if (match) {
-        pos = regex.lastIndex;
-        state = regex;
-
-        if (
-          state === openTag && match.groups?.tagname &&
-          rawTextElements.test(match.groups.tagname)
-        ) {
-          rawTextMode = true;
-        }
-        if (
-          state === endTag && match.groups?.tagname &&
-          rawTextElements.test(match.groups.tagname)
-        ) {
-          rawTextMode = false;
-        }
-
-        return;
-      }
-    }
-  };
-
-  while (pos !== input.length) {
-    if (
-      !state ||
-      [text, rawText, comment, cdata, closeOpenTag, endTag].includes(state)
-    ) {
-      const textOrRawText = rawTextMode ? rawText : text;
-      admissibleNextStates = [textOrRawText, comment, cdata, endTag, openTag];
-    } else if (
-      [openTag, attributeValueQuoted, attributeValueUnquoted, booleanAttribute]
-        .includes(state)
-    ) {
-      admissibleNextStates = [closeOpenTag, attribute, booleanAttribute];
-    } else if (state === attribute) {
-      admissibleNextStates = [
-        attributeValueQuoted,
-        attributeValueUnquoted,
-      ];
-    }
-
-    nextState();
-  }
-
-  return input;
-};
+import { nanoId } from "./utils.ts";
 
 export const html: TemplateTag = (
   strings,
@@ -86,33 +12,31 @@ export const html: TemplateTag = (
 ) => {
   let innerHTML = "";
 
-  const boundariesMap = new Map<number, Boundary<any>>();
-  const attachmentsMap = new Map<string, (node: Node) => any>();
+  const boundariesMap = new Map<number, Boundary>();
+  const attachmentsMap = new Map<string, Attachment>();
 
   for (let index = 0; index < values.length; index++) {
     const string = strings[index]!;
 
-    innerHTML += incrementalParse(string);
+    innerHTML += string;
     const data = values[index];
 
-    if (state !== attribute) {
-      if (
-        isSignal(data) ||
-        isComponent(data) ||
-        typeof data === "function"
-      ) {
-        const boundary = new Boundary(data);
-        boundariesMap.set(boundary.id, boundary);
-
-        innerHTML += String(boundary);
-      } else {
-        innerHTML += String(data);
-      }
-    } else if (
-      [openTag, attributeValueQuoted, attributeValueUnquoted, booleanAttribute]
-        .includes(state)
+    if (
+      isSignal(data) ||
+      isComponent(data) ||
+      typeof data === "function"
     ) {
-      innerHTML += ` x-attach='${match?.groups?.name}' `;
+      const boundary = new Boundary(data);
+      boundariesMap.set(boundary.id, boundary);
+
+      innerHTML += String(boundary);
+    } else if (isAttachment(data)) {
+      const id = nanoId();
+      attachmentsMap.set(id, data);
+
+      innerHTML += ` attachment-${id} `;
+    } else {
+      innerHTML += String(data);
     }
   }
 
@@ -143,6 +67,16 @@ export const html: TemplateTag = (
 
     boundary.end = comment;
     boundary.render();
+  }
+
+  for (const [id, attachment] of attachmentsMap.entries()) {
+    const element = content.querySelector(`[attachment-${id}]`);
+    assertExists(element, `No element found with attachement id ${id}`);
+
+    element.removeAttribute(`attachment-${id}`);
+    effect(() => {
+      attachment(element);
+    });
   }
 
   return content;
