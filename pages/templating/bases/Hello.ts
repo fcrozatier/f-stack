629e38b063bb2f58472879a381f07bc85ffcb701
fcrozatier <frederic.crozatier@protonmail.com>
2025-07-29 12:51:14 +0200

    format

--- pages/templating/bases/Hello.ts
+++ pages/templating/bases/Hello.ts
@@ -1,32 +1,35 @@
 import { attach } from "$client/attachement.ts";
 import { component } from "$client/component.ts";
 import { html } from "$client/html.ts";
-import styles from './hello.css' with { type: "css" };
+import styles from "./hello.css" with { type: "css" };
 
-document.adoptedStyleSheets.push(styles)
+document.adoptedStyleSheets.push(styles);
 
-const Nested = component(()=>{
-  return html`<p data-scope=2>Another</p>`
-})
+const Nested = component(() => {
+  return html`
+    <p data-scope="2">Another</p>
+  `;
+});
 
 export const Hello = component(() => {
   // Add data
   const name = "World";
-  const src = "https://images.dog.ceo/breeds/terrier-patterdale/dog-1268559_640.jpg"
+  const src =
+    "https://images.dog.ceo/breeds/terrier-patterdale/dog-1268559_640.jpg";
 
   return html`
-    <div data-scope=1>
-    <h1>Hello ${name.toUpperCase()}</h1>
+    <div data-scope="1">
+      <h1>Hello ${name.toUpperCase()}</h1>
 
-    <img ${attach((img: HTMLImageElement) => {
-      // dynamic attributes
-      img.src = src
-      img.alt = `${name} dances`;
-    })}" />
+      <img ${attach((img: HTMLImageElement) => {
+        // dynamic attributes
+        img.src = src;
+        img.alt = `${name} dances`;
+      })} />
 
-    <p>A paragraph</p>
+      <p>A paragraph</p>
 
-    ${Nested}
+      ${Nested}
     </div>
   `;
 });
