b6ad52b0f876210f64be4478dceee2ec1cd2faaa
fcrozatier <frederic.crozatier@protonmail.com>
2025-07-29 15:36:41 +0200

    move counter

---
+++ pages/reactivity/counter/Counter.ts
@@ -0,0 +1,20 @@
+import { attach } from "$client/attachement.ts";
+import { component } from "$client/component.ts";
+import { html } from "$client/html.ts";
+import { computed, state } from "$client/signals.ts";
+
+type Props = { initial: number };
+
+export const Counter = component<Props>(({ initial = 0 }) => {
+  const count = state(initial);
+  const isEven = computed(() => (count.value & 1) === 0);
+
+  return html`
+    <button ${attach((b) => {
+      b.addEventListener("click", () => count.value++);
+    })}>Click</button>
+
+    <div>The count is: ${count}</div>
+    <div>Is even: ${isEven}</div>
+  `;
+});
