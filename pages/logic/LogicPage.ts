1020fa2c879cc497ed71fa8002a6417aab0856a2
fcrozatier <frederic.crozatier@protonmail.com>
2025-08-26 12:41:13 +0200

    rewrite the logic page

--- pages/logic/LogicPage.ts
+++ pages/logic/LogicPage.ts
@@ -1,21 +1,33 @@
 import { html } from "$client/html.ts";
-import { computed, state } from "$client/reactivity/signals.ts";
-import { attach } from "$client/sinks.ts";
+import { reactive } from "$client/reactivity/reactive.ts";
+import { on } from "$client/sinks.ts";
 
 export const LogicPage = () => {
-  const count = state(0);
+  const count = reactive({ value: 0 });
 
   return html`
-    <button ${attach((b) => {
-      b.addEventListener("click", () => {
-        count.value++;
-      });
-    })}>Clicked ${count} ${count.value === 1 ? "time" : "times"}</button>
+    <button ${on({
+      click: () => count.value++,
+    })}>Clicked ${count} ${reactive({
+      get value() {
+        return count.value === 1 ? "time" : "times";
+      },
+    })}</button>
 
-    ${computed(() =>
-      count.value > 10 ? "Too big" : count.value < 5 ? count.value : html`
-        <span>${count} is between 5 and 10</span>
-      `
-    )}
+    <!-- Can we have smart "partial recomputation" to avoid recreating the whole span
+      on every update? -->
+    ${reactive({
+      get value() {
+        if (count.value > 10) {
+          return "Too big";
+        } else if (count.value < 5) {
+          return count.value;
+        } else {
+          return html`
+            <span>${count} is between 5 and 10</span>
+          `;
+        }
+      },
+    })}
   `;
 };
