b98725f129cb965925b5f37c19610583bc398752
fcrozatier <frederic.crozatier@protonmail.com>
2025-10-10 14:59:16 +0200

    refactor counter

--- pages/reactivity/1-state-and-derived/Counter.ts
+++ pages/reactivity/1-state-and-derived/Counter.ts
@@ -1,20 +1,20 @@
-import { attach } from "$client/sinks.ts";
-
 import { html } from "$client/html.ts";
-import { computed, state } from "$client/reactivity/signals.ts";
+import { reactive } from "$client/reactivity/reactive.ts";
+import { on, text } from "$client/sinks.ts";
 
 type Props = { initial: number };
 
 export const Counter = (props = { initial: 0 } satisfies Props) => {
-  const count = state(props.initial);
-  const isEven = computed(() => (count.value & 1) === 0);
+  const state = reactive({
+    count: props.initial,
+    get isEven() {
+      return (this.count % 2) === 0;
+    },
+  });
 
   return html`
-    <button ${attach((b) => {
-      b.addEventListener("click", () => count.value++);
-    })}>Click</button>
-
-    <div>The count is: ${count}</div>
-    <div>Is even: ${isEven}</div>
+    <button ${on({ click: () => state.count++ })}>Click</button>
+    <div>The count is: ${text(state, "count")}</div>
+    <div>Is even: ${text(state, "isEven")}</div>
   `;
 };
