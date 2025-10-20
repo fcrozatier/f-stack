83d9818d38cfa09946cb817859484acfb99c2d91
fcrozatier <frederic.crozatier@protonmail.com>
2025-10-20 16:41:22 +0200

    display more data in the benchmark

--- playground/benchmark.ts
+++ playground/benchmark.ts
@@ -1,16 +1,26 @@
 import { html } from "../packages/reflow/src/html.ts";
 import { Counter } from "./pages/reactivity/1-state-and-derived/Counter.ts";
 
+const COUNT = 1000;
+
 export const Benchmark = () => {
   const start = performance.now();
 
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < COUNT; i++) {
     Counter();
   }
 
   const end = performance.now();
 
+  const duration = (end - start).toFixed(2);
+  const avg = (+duration / COUNT).toFixed(2);
+
   return html`
-    Benchmark done in ${(end - start).toFixed(2)}ms
+    <p>
+      Benchmark done in ${duration}ms
+    </p>
+    <p>
+      Average render time ${avg}ms
+    </p>
   `;
 };
