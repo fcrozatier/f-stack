00febb64f794941355f430f490281ba2cf5e6034
fcrozatier <frederic.crozatier@protonmail.com>
2025-11-06 13:39:54 +0100

    add prop test

---
+++ packages/reflow/tests/prop/index.js
@@ -0,0 +1,17 @@
+import { reactive } from "@f-stack/functorial";
+import { html, on, prop } from "@f-stack/reflow";
+
+export default () => {
+  const bool = reactive({ value: true });
+
+  return html`
+    <button ${on({ click: () => bool.value = !bool.value })}>toggle</button>
+
+    <input type="checkbox" ${prop({
+      // @ts-ignore
+      get indeterminate() {
+        return bool.value;
+      },
+    })} data-testid="input"></span>
+  `;
+};
